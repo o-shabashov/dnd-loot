41a23e274d9a2628a80a90a90ece18cf64c2a5b1
Oleg Shabashov <oleg.shabashov@searchanise.io>
2024-05-01 09:26:45 +0700

    Normalie db data

--- app/Console/Commands/NormalizeDicePlusRolls.php
+++ app/Console/Commands/NormalizeDicePlusRolls.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Item;
+use App\Models\Perishable;
 use App\Models\Prefix;
 use App\Models\Suffix;
 use Illuminate\Console\Command;
@@ -30,17 +31,34 @@ class NormalizeDicePlusRolls extends Command
     public function handle()
     {
         foreach (Item::all() as $model) {
+            $model->cost = Str::replace("\xc2\xa0", '', $model->cost);
             $model->dice_raw = Str::replace('+', '-100', $model->dice_raw);
+            $model->effect = Str::replace('Id', '1d', $model->effect);
+            $model->effect = Str::replace('ld', '1d', $model->effect);
+            $model->save();
+        }
+
+        foreach (Perishable::all() as $model) {
+            $model->cost = Str::replace("\xc2\xa0", '', $model->cost);
+            $model->dice_raw = Str::replace('+', '-100', $model->dice_raw);
+            $model->effect = Str::replace('Id', '1d', $model->effect);
+            $model->effect = Str::replace('ld', '1d', $model->effect);
             $model->save();
         }
 
         foreach (Prefix::all() as $model) {
+            $model->cost = Str::replace("\xc2\xa0", '', $model->cost);
             $model->dice_raw = Str::replace('+', '-100', $model->dice_raw);
+            $model->effect = Str::replace('Id', '1d', $model->effect);
+            $model->effect = Str::replace('ld', '1d', $model->effect);
             $model->save();
         }
 
         foreach (Suffix::all() as $model) {
+            $model->cost = Str::replace("\xc2\xa0", '', $model->cost);
             $model->dice_raw = Str::replace('+', '-100', $model->dice_raw);
+            $model->effect = Str::replace('Id', '1d', $model->effect);
+            $model->effect = Str::replace('ld', '1d', $model->effect);
             $model->save();
         }
     }
