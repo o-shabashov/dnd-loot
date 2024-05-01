0c60249f754f19fd5c3a72c713c94ccba87e9ced
Oleg Shabashov <oleg.shabashov@searchanise.io>
2024-05-01 09:05:13 +0700

    Normalize dice_roll

--- app/Console/Commands/NormalizeDicePlusRolls.php
+++ app/Console/Commands/NormalizeDicePlusRolls.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Item;
+use App\Models\Prefix;
+use App\Models\Suffix;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class NormalizeDicePlusRolls extends Command
 {
@@ -25,6 +29,19 @@ class NormalizeDicePlusRolls extends Command
      */
     public function handle()
     {
-        //
+        foreach (Item::all() as $model) {
+            $model->dice_raw = Str::replace('+', '-100', $model->dice_raw);
+            $model->save();
+        }
+
+        foreach (Prefix::all() as $model) {
+            $model->dice_raw = Str::replace('+', '-100', $model->dice_raw);
+            $model->save();
+        }
+
+        foreach (Suffix::all() as $model) {
+            $model->dice_raw = Str::replace('+', '-100', $model->dice_raw);
+            $model->save();
+        }
     }
 }
