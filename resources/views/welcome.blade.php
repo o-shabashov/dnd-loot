65b09652a4cf802d84cd194d7576cef1c54c1657
Oleg Shabashov <oleg.shabashov@searchanise.io>
2024-05-06 10:59:31 +0700

    ucfirst item name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -870,7 +870,7 @@
                     <div
                         class="items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                         style="text-align: center">
-                        <h1 class="text-xl font-semibold text-black dark:text-white">{{$item?->group?->name}}</h1>
+                        <h1 class="text-xl font-semibold text-black dark:text-white">{{ucfirst($item?->group?->name)}}</h1>
                     </div>
 
                     <div
@@ -889,19 +889,19 @@
                         <div class="pt-3 sm:pt-5">
                             @if($prefix?->name)
                                 <h2 class="text-xl font-semibold text-black dark:text-white">
-                                    {{ $prefix->name }}
+                                    {{ ucfirst($prefix->name) }}
                                 </h2>
                             @endif
 
                             @if($prefix?->effect)
                                 <p class="mt-4 text-xl" style="color: seagreen;">
-                                    {{ $prefix->effect }}
+                                    {{ ucfirst($prefix->effect) }}
                                 </p>
                             @endif
 
                             @if($prefix?->cost)
                                 <p class="mt-4 text-xl" style="color: bisque;">
-                                    {{ $prefix->cost }}💰
+                                    {{ ucfirst($prefix->cost) }}💰
                                 </p>
                             @endif
                         </div>
@@ -927,18 +927,18 @@
 
                         <div class="pt-3 sm:pt-5">
                             @if($item?->name)
-                                <h2 class="text-xl font-semibold text-black dark:text-white">{{ $item->name }}</h2>
+                                <h2 class="text-xl font-semibold text-black dark:text-white">{{ ucfirst($item->name) }}</h2>
                             @endif
 
                             @if($item?->effect)
                                 <p class="mt-4 text-xl">
-                                    {{ $item->effect }}
+                                    {{ ucfirst($item->effect) }}
                                 </p>
                             @endif
 
                             @if($item?->cost)
                                 <p class="mt-4 text-xl" style="color: bisque;">
-                                    {{ $item->cost }}💰
+                                    {{ ucfirst($item->cost) }}💰
                                 </p>
                             @endif
                         </div>
@@ -961,19 +961,19 @@
                         <div class="pt-3 sm:pt-5">
                             @if($suffix?->name)
                                 <h2 class="text-xl font-semibold text-black dark:text-white">
-                                    {{ $suffix->name }}
+                                    {{ ucfirst($suffix->name) }}
                                 </h2>
                             @endif
 
                             @if($suffix?->effect)
                                 <p class="mt-4 text-xl" style="color: darkcyan;">
-                                    {{ $suffix->effect }}
+                                    {{ ucfirst($suffix->effect) }}
                                 </p>
                             @endif
 
                             @if($suffix?->cost)
                                 <p class="mt-4 text-xl" style="color: bisque;">
-                                    {{ $suffix->cost }}💰
+                                    {{ ucfirst($suffix->cost) }}💰
                                 </p>
                             @endif
                         </div>
