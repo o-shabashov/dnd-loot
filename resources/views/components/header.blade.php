94b9632c61d3c68224112ba61df1e4126fd6f8fd
Oleg Shabashov <oleg.shabashov@searchanise.io>
2024-06-17 13:42:11 +1000

    Init tailwind through the vite

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,15 @@
 <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-    <div class="flex lg:justify-center lg:col-start-2" style="cursor: pointer">
-        <h2 onClick="window.location.reload();">🔄 Dungeon: {{ $dungeonLevel }}</h2>
+    <div class="flex lg:justify-center lg:col-start-2">
+        <span onClick="window.location.reload();" class="gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]" style="text-align: center">🔄 {{ $dungeonLevel }}</span>
         &nbsp;
-        <a href="/loot?dungeon_level={{ $dungeonLevel }}">Loot</a>
+        <a href="/loot?dungeon_level={{ $dungeonLevel }}" class="gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]" style="text-align: center">💰Loot</a>
         &nbsp;
-        <a href="/smith?dungeon_level={{ $dungeonLevel }}">Smith</a>
+        <a href="/smith?dungeon_level={{ $dungeonLevel }}" class="gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]" style="text-align: center">🛠️ Smith</a>
         &nbsp;
-        <a href="/medic?dungeon_level={{ $dungeonLevel }}">Heal</a>
+        <a href="/medic?dungeon_level={{ $dungeonLevel }}" class="gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]" style="text-align: center">🏥 Heal</a>
         &nbsp;
-        <a href="/witch?dungeon_level={{ $dungeonLevel }}">Witch</a>
+        <a href="/witch?dungeon_level={{ $dungeonLevel }}" class="gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]" style="text-align: center">🧙‍  Witch</a>
         &nbsp;
-        <a href="/purge-cache">Cache</a>
+        <a href="/purge-cache" class="gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]" style="text-align: center">🚫 Cache</a>
     </div>
 </header>
